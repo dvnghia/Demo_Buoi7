df939f47c4f3bf0264d94e2ed3cc5907104736a7
dvnghia <nghia6122004@gmail..com>
2026-03-26 14:22:59 +0700

    first commit

---
+++ src/main/java/com/example/DuongVietNghia/DuongVietNghiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.DuongVietNghia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuongVietNghiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuongVietNghiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DuongVietNghia/DuongVietNghiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DuongVietNghia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuongVietNghiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
